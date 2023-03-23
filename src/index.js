8049c205f664d4983ac732837e262b1ee74f5478
UTSAV GUPTA <utsavgupta893@gmail.com>
2023-03-24 01:28:56 +0530

    Setup Basic body-parser

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 const express=require('express');
+const bodyParser=require('body-parser');
 const {PORT}=require('./config/serverconfig');
+
+
+
 
 const setupAndStartServer=async ()=>{
     const app=express();
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended:true}));
     app.listen(PORT,()=>{
         //.dotenv helps to setup the environment files
         console.log(`Server Started at ${PORT}`);
+
         
     })
     
